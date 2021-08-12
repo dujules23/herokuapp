d209638095d1b139cce0817901fcbd6b3cf9a552
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-11 21:26:04 -0400

    monolith push

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // declare dotenv with the config method
-
 const dotenv = require("dotenv").config()
-const express = require("express")
 
+const express = require("express")
+// after installation, bring in cors here.
 const cors = require("cors")
+
 const app = express()
+
+
 // gives access to heroku's port
 const port = process.env.PORT || 9000
+
+// path allows you to create paths
+const path = require("path")
 
 // Makes it so only one website can access api data
 app.use(cors())
 // allows you to grab json and use it through out the app
 app.use(express.json())
+// tells express to access a static page (html, tc.)
+// put in one parameter along with the working path
+// this specifically goes to the home page
+app.use(express.static(path.join(__dirname, "client/build")))
 
 
 // use _ if you don't want to use the req object
@@ -19,6 +29,11 @@ app.use("/api/*", (_, res) =>{
   res.json({
     data: "The API is working!"
   })
+})
+
+// handles incorrect endpoints
+app.use("*", (__,res) =>{
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
 app.listen(port, () => {
